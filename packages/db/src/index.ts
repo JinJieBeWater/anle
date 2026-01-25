c7b512473b72ea4e7d391826a0da0e0ac199f870
JinJieBeWater <cjj20030813@qq.com>
2026-01-25 09:59:30 +0800

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,6 @@
+import { env } from "@anle/env/server";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import * as schema from "./schema";
+
+export const db = drizzle(env.DATABASE_URL, { schema });
